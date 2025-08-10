954eb51837ee608b3e87f99ec3b63bcfa0e9ee94
Emily Wind <emlywind@gmail.com>
2025-08-10 12:49:21 -0400

    Fix EasyDelete for 11.2.0

--- src/EasyDelete.lua
+++ src/EasyDelete.lua
@@ -1,11 +1,9 @@
 OnPlayerLogin(function()
-
-hooksecurefunc(
-  StaticPopupDialogs["DELETE_GOOD_ITEM"],
-  "OnShow",
-  function(s)
-    s.editBox:SetText(DELETE_ITEM_CONFIRM_STRING)
-  end
-)
-
+  hooksecurefunc(
+    StaticPopupDialogs["DELETE_GOOD_ITEM"],
+    "OnShow",
+    function(s)
+      (s.EditBox or s.editBox):SetText(DELETE_ITEM_CONFIRM_STRING)
+    end
+  )
 end)
