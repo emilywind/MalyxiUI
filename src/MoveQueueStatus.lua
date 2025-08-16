5feae1a5f3fc8853fb92f477954f9ed54b20c2da
Emily Wind <emlywind@gmail.com>
2025-08-15 20:47:10 -0400

    If positions are nil, do not move

--- src/MoveQueueStatus.lua
+++ src/MoveQueueStatus.lua
@@ -5,7 +5,7 @@ OnPlayerLogin(function()
   local inQueue = false
 
   -- Queue Status Icon
-  local function queueIconPos(frame, layoutName, point, x, y)
+  local function queueIconPos(_, layoutName, point, x, y)
     db.queueicon.point = point
     db.queueicon.x = x
     db.queueicon.y = y
@@ -29,19 +29,18 @@ OnPlayerLogin(function()
   end)
 
   LEM:RegisterCallback('layout', function(layoutName)
+    if db.queueicon.x == nil or db.queueicon.y == nil then return end
     QueueStatusButton:SetPoint(db.queueicon.point, UIParent, db.queueicon.point, db.queueicon.x, db.queueicon.y)
   end)
 
-  local function QueueStatusButton_Reposition()
+  hooksecurefunc(QueueStatusButton, "UpdatePosition", function()
+    if db.queueicon.x == nil or db.queueicon.y == nil then return end
+
     if C_AddOns.IsAddOnLoaded("EditModeExpanded") then return end
     QueueStatusButton:SetParent(UIParent)
     QueueStatusButton:SetFrameLevel(1)
     QueueStatusButton:SetScale(0.8, 0.8)
     QueueStatusButton:ClearAllPoints()
     QueueStatusButton:SetPoint(db.queueicon.point, UIParent, db.queueicon.point, db.queueicon.x, db.queueicon.y)
-  end
-
-  hooksecurefunc(QueueStatusButton, "UpdatePosition", function()
-    QueueStatusButton_Reposition()
   end)
 end)
