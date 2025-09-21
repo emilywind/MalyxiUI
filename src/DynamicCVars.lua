4b8f52a8579d7814081872500e04a5a10c9e9835
Emily Wind <emlywind@gmail.com>
2025-09-21 10:14:42 -0400

    This relies on the other setting

--- src/DynamicCVars.lua
+++ src/DynamicCVars.lua
@@ -3,7 +3,7 @@ local SetCVar = C_CVar.SetCVar
 OnEvent("PLAYER_ENTERING_WORLD", function()
     local instanceInfo = GetInstanceData()
 
-    if instanceInfo.isInPvE and EUIDB.nameplateHideFriendsPve then
+    if instanceInfo.isInPvE and EUIDB.nameplateShowFriends and EUIDB.nameplateHideFriendsPve then
       SetCVar("nameplateShowFriends", 0)
     elseif EUIDB.nameplateShowFriends then
       SetCVar("nameplateShowFriends", 1)
