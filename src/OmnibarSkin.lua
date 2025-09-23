007ac2ed22280bf936fc763d179fd69d15dfc982
Emily Wind <emlywind@gmail.com>
2025-09-23 09:20:24 -0400

    Add typing

--- src/OmnibarSkin.lua
+++ src/OmnibarSkin.lua
@@ -1,7 +1,9 @@
 OnPlayerLogin(function()
   if not OmniBar_StartCooldown then return end
 
-  hooksecurefunc('OmniBar_StartCooldown', function(_, icon)
+  hooksecurefunc('OmniBar_StartCooldown',
+  ---@param icon Texture
+  function(_, icon)
     ApplyEuiBackdrop(icon)
   end)
 end)
